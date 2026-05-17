4b2b8f772a8b89c49cda1df488e63d5f87803a0f
WeberTechDevs <webertechdevs@gmail.com>
2026-05-17 10:59:50 +0300

    Update App.jsx
    update bundles 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Cyber from "./pages/Cyber";
 import Hustle from "./pages/Hustle";
 import Academy from "./pages/Academy";
 import Electronics from "./pages/Electronics";
-import Bundles from "./pages/Bundles";
 import Dev from "./pages/Dev";
 import NotFound from "./pages/NotFound";
+
+// This component redirects to your Firebase app immediately
+function BundlesRedirect() {
+  useEffect(() => {
+    window.location.replace("https://bundles.webertech.co.ke");
+  }, []);
+  return null;
+}
 
 export default function App() {
   return (
@@ -17,9 +25,8 @@ export default function App() {
         <Route path="/cyber" element={<Cyber />} />
         <Route path="/hustle" element={<Hustle />} />
         <Route path="/academy" element={<Academy />} />
-        <Route path="/bundles"
-element={<Bundles />} />
         <Route path="/electronics" element={<Electronics />} />
+        <Route path="/bundles" element={<BundlesRedirect />} />
         <Route path="/dev" element={<Dev />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
